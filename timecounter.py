4871bd5113ff1fde4d694a0755f6350b7eb469f6
lunatik <moon.nut@yandex.ru>
2025-06-26 23:45:43 +0300

    update

--- timecounter.py
+++
@@ -1,7 +0,0 @@
-from worktime.counter import TimeCounter
-from worktime.cli import main
-
-__all__ = ["TimeCounter", "main"]
-
-if __name__ == "__main__":
-    main()
--- timecounter_gui.py
+++
@@ -1,4 +0,0 @@
-from worktime.gui import main
-
-if __name__ == "__main__":
-    main()
